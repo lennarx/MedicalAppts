8da875836985fe61fd85497170c4dc17cfffbbb5
Franco Redoni <franco.redoni@gmail.com>
2025-04-21 22:31:44 -0300

    Connection string as env variable

--- MedicalAppts.Api/Program.cs
+++ MedicalAppts.Api/Program.cs
@@ -1,10 +1,13 @@
 using MedicalAppts.Api.Configurations;
+using MedicalAppts.Api.Utils.Seeders;
+using MedicalAppts.Infrastructure;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddServiceConfigs(builder);
-
 var app = builder.Build();
 
 app.UseSwagger(options =>
@@ -16,6 +19,20 @@ app.UseSwaggerUI(c =>
     c.SwaggerEndpoint("/swagger/v1/swagger.json", "Medical API v1");
     c.RoutePrefix = "swagger";
 });
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<MedicalApptsDbContext>();
+    if (dbContext.Database.GetPendingMigrations().Any())
+    {
+        dbContext.Database.Migrate();
+    }
+    if (app.Environment.IsDevelopment())
+    { 
+        await DbSeeder.SeedAsync(dbContext);
+    }
+}
+
 
 app.UseAuthorization();
 app.MapControllers();
