62249165a61bcb61356d149a29e9b960a167ba9f
Franco Redoni <franco.redoni@gmail.com>
2025-04-22 01:03:45 -0300

    New errors for appointment creation

--- MedicalAppts.Core/Errors/GenericErrors.cs
+++ MedicalAppts.Core/Errors/GenericErrors.cs
@@ -5,7 +5,9 @@
         public static readonly Error AppointmentNotFound = new Error(400, "Appointment not found for provided criteria");
         public static readonly Error NoSpecialtyDoctorsFound = new Error(404, "No doctors found for required specialty");
         public static readonly Error AppointmentRequestError = new Error(400, "Error while processing appointment request");
+        public static readonly Error AppointmentAlreadyExists = new Error(400, "Appointment already exists");
         public static readonly Error AppointmentCancellationError = new Error(400, "An error occurred while attempting to cancel the appointment");
+        public static readonly Error AppointmentOutOfTimeFrame = new Error(400, "Appointment is not within the doctor attention time frame");
         public static readonly Error DoctorCreationError = new Error(400, "An error occurred while attempting to create the doctor");
         public static readonly Error PatientCreationError = new Error(400, "An error occurred while attempting to create the patient");
     }
