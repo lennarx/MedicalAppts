fafd5a687cb5462a672374bfc32ba8de3ebec50a
Franco Redoni <franco.redoni@gmail.com>
2025-04-22 17:09:47 -0300

    New errors for handling appointment update errors

--- MedicalAppts.Core/Errors/GenericErrors.cs
+++ MedicalAppts.Core/Errors/GenericErrors.cs
@@ -2,11 +2,13 @@
 {
     public class GenericErrors
     {
-        public static readonly Error AppointmentNotFound = new Error(400, "Appointment not found for provided criteria");
+        public static readonly Error AppointmentNotFound = new Error(404, "Appointment not found for provided criteria");
         public static readonly Error NoSpecialtyDoctorsFound = new Error(404, "No doctors found for required specialty");
         public static readonly Error AppointmentRequestError = new Error(400, "Error while processing appointment request");
         public static readonly Error AppointmentAlreadyExists = new Error(400, "Appointment already exists");
         public static readonly Error AppointmentCancellationError = new Error(400, "An error occurred while attempting to cancel the appointment");
+        public static readonly Error AppointmentUpdateError = new Error(400, "An error occurred while attempting to reschedule the appointment");
+        public static readonly Error AppointmentNotOwnedByPatient = new Error(400, "The patient is not the owner of the appointment");
         public static readonly Error AppointmentOutOfTimeFrame = new Error(400, "Appointment is not within the doctor attention time frame");
         public static readonly Error DoctorCreationError = new Error(400, "An error occurred while attempting to create the doctor");
         public static readonly Error PatientCreationError = new Error(400, "An error occurred while attempting to create the patient");
