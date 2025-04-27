1044ca56cd3dc3145cb89a15118a8eda80f87078
Franco Redoni <franco.redoni@gmail.com>
2025-04-27 19:54:19 -0300

    Pending Migrations applications isolated

---
+++ MedicalAppts.Api/Configurations/MigrationsConfigurations.cs
@@ -0,0 +1,31 @@
+﻿using MedicalAppts.Api.Utils.Seeders;
+using MedicalAppts.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedicalAppts.Api.Configurations
+{
+    public static class MigrationsConfigurations
+    {
+        public static async Task ApplyMigrationsIfNeeded(this IServiceProvider serviceProvider, IWebHostEnvironment env)
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<MedicalApptsDbContext>();
+                var providerName = dbContext.Database.ProviderName;
+
+                if (!string.IsNullOrEmpty(providerName) && providerName != "Microsoft.EntityFrameworkCore.InMemory")
+                {
+                    if (dbContext.Database.GetPendingMigrations().Any())
+                    {
+                        dbContext.Database.Migrate();
+                    }
+                }
+
+                if (!env.IsDevelopment())
+                {
+                    await DbSeeder.SeedAsync(dbContext);
+                }
+            }
+        }
+    }
+}
