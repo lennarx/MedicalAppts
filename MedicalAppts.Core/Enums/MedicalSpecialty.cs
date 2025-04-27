ce2f4d209e04b7029617dcb2bd7ceac22f72ae61
Franco Redoni <franco.redoni@gmail.com>
2025-04-27 19:57:03 -0300

    More support for enum parsing

--- MedicalAppts.Core/Enums/MedicalSpecialty.cs
+++ MedicalAppts.Core/Enums/MedicalSpecialty.cs
@@ -1,14 +1,24 @@
-﻿namespace MedicalAppts.Core.Enums
+﻿using System.Runtime.Serialization;
+
+namespace MedicalAppts.Core.Enums
 {
     public enum MedicalSpecialty
     {
+        [EnumMember(Value = nameof(CARDIOLOGIST))]
         CARDIOLOGIST,
+        [EnumMember(Value = nameof(CLINICAL))]
         CLINICAL,
+        [EnumMember(Value = nameof(GASTROENTEROLOGIST))]
         GASTROENTEROLOGIST,
+        [EnumMember(Value = nameof(NEUROLOGIST))]
         NEUROLOGIST,
+        [EnumMember(Value = nameof(DERMATOLOGIST))]
         DERMATOLOGIST,
+        [EnumMember(Value = nameof(ORTHOPEDIC))]
         ORTHOPEDIC,
+        [EnumMember(Value = nameof(PEDIATRICIAN))]
         PEDIATRICIAN,
+        [EnumMember(Value = nameof(PSYCHIATRIST))]
         PSYCHIATRIST
     }
 }
