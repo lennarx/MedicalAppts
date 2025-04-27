4f4794aa7df89e203a587650139fc6d25808444a
Franco Redoni <franco.redoni@gmail.com>
2025-04-27 19:56:48 -0300

    Program as a class

--- MedicalAppts.Api/Program.cs
+++ MedicalAppts.Api/Program.cs
@@ -1,42 +1,35 @@
 using MedicalAppts.Api.Configurations;
-using MedicalAppts.Api.Utils.Seeders;
-using MedicalAppts.Infrastructure;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.OpenApi;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Configuration.AddEnvironmentVariables();
-builder.Services.AddServiceConfigs(builder);
-var app = builder.Build();
-
-app.UseSwagger(options =>
+public class Program
 {
-    options.OpenApiVersion = OpenApiSpecVersion.OpenApi3_0;
-});
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Medical API v1");
-    c.RoutePrefix = "swagger";
-});
-
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<MedicalApptsDbContext>();
-    if (dbContext.Database.GetPendingMigrations().Any())
+    public static async Task Main(string[] args)
     {
-        dbContext.Database.Migrate();
-    }
-    if (app.Environment.IsDevelopment())
-    { 
-        await DbSeeder.SeedAsync(dbContext);
-    }
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Configuration.AddEnvironmentVariables();
+        builder.Services.AddServiceConfigs(builder);
+        var app = builder.Build();
+
+        app.UseSwagger(options =>
+        {
+            options.OpenApiVersion = OpenApiSpecVersion.OpenApi3_0;
+        });
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Medical API v1");
+            c.RoutePrefix = "swagger";
+        });
+
+        await app.Services.ApplyMigrationsIfNeeded(app.Environment);
+
+        app.MapControllers();
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.Run();
+    }    
 }
 
 
-app.MapControllers();
-app.UseHttpsRedirection();
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
-app.Run();
